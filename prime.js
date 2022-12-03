e7cf6067802cbe845c5a9bdf776497c4b2a7df53
nitin chainchlani <nitin.chainchlani534@gmail.com>
2022-12-03 18:39:29 +0530

    changes maid by nitin

---
+++ prime.js
@@ -0,0 +1,18 @@
+// Prime number code.
+
+//check weather a number is prime or not
+let i=2;
+
+while(i<num){
+if(num%i==0){
+console.log("No)
+break;
+}
+i++;
+
+}
+if (i===num){
+console.log("yes)
+
+}
+
