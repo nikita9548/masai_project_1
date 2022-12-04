0c34f566d7dd416be10843a3df8997bd8d1d7527
nikita9548 <119389813+nikita9548@users.noreply.github.com>
2022-12-04 10:54:58 +0530

    Revert "changes maid by nitin"

---
+++ login.js
@@ -0,0 +1,17 @@
+// check weather a number is prime or not//
+
+
+1. let num= 5;
+2. let count= 0;
+3. for (let i=0; i<num;i++){
+4.}
+5. if(num%i==1){
+6. }
+7. console.log("it is a prime number")
+8. else{
+9. console.log("it is not a prime number")
+10.
+11.
+12.
+13.
+
